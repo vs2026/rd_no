77670ee372e96a9893d93c73efee42efb6d4a3e7
vs2026 <129305288+vs2026@users.noreply.github.com>
2025-10-19 04:59:32 +0800

    Update rendezvous server and public key constants

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.arphy.top"];
+pub const RS_PUB_KEY: &str = "xMPbjR5E4c4utTUkAxnbP3YtWaOeDleT433ARIexdO4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
